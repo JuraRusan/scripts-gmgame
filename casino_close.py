e28c7033c3e96c78f87b52c2bd969dd39719e7b8
jurarusan <rusanjura@gmail.com>
2022-10-17 09:36:37 +0300

    first commit

---
+++ casino_close.py
@@ -0,0 +1,167 @@
+import os
+import time
+# ----------------------------------
+command = 'echo "npc2 select 29" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3818.5 -5.5 6829.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 30" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3815.5 -5.5 6829.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 31" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3812.5 -5.5 6829.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 32" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3809.5 -5.5 6829.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 33" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3818.5 -5.5 6832.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 34" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3815.5 -5.5 6832.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 35" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3812.5 -5.5 6832.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 36" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3809.5 -5.5 6832.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 37" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3818.5 -5.5 6835.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 38" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3815.5 -5.5 6835.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 39" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3812.5 -5.5 6835.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 40" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3809.5 -5.5 6835.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 41" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3818.5 -5.5 6838.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 42" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3815.5 -5.5 6838.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 57" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3812.5 -5.5 6838.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 58" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3809.5 -5.5 6838.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+# ----------------------------------
+command = 'echo "npc2 select 43" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3821.5 -5.5 6829.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 44" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3824.5 -5.5 6829.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+# ----------------------------------
+command = 'echo "npc2 select 55" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3827.5 -5.5 6829.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 56" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3830.5 -5.5 6829.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+# ----------------------------------
+command = 'echo "npc2 select 45" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3824.5 -5.5 6832.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 46" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3827.5 -5.5 6832.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 47" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3830.5 -5.5 6832.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 48" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3833.5 -5.5 6832.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 49" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3824.5 -5.5 6835.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 50" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3827.5 -5.5 6835.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 51" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3830.5 -5.5 6835.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 52" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3833.5 -5.5 6835.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 53" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3824.5 -5.5 6838.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 54" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3827.5 -5.5 6838.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 59" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3830.5 -5.5 6838.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 60" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -3833.5 -5.5 6838.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+# ----------------------------------
---
+++ casino_open.py
@@ -0,0 +1,167 @@
+import os
+import time
+# ----------------------------------
+command = 'echo "npc2 select 29" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 74.5 42.5 745.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 30" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 8.5 42.5 774.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 31" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 15.5 45.5 767.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 32" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 54.5 43.5 763.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 33" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 74.5 42.5 774.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 34" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 8.5 42.5 745.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 35" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 15.5 43.5 752.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 36" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 54.5 43.5 756.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 37" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 14.5 50.5 769.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 38" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 14.5 50.5 750.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 39" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -7.5 51.5 762.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 40" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto -7.5 51.5 757.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 41" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 68.5 42.5 753.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 42" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 68.5 42.5 766.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 57" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 76.5 50.5 757.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 58" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 76.5 50.5 762.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+# ----------------------------------
+command = 'echo "npc2 select 43" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 68.5 42.5 760" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 44" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 10.5 50.5 760" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+# ----------------------------------
+command = 'echo "npc2 select 55" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 6.5 42.5 757.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 56" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 6.5 42.5 762.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+# ----------------------------------
+# command = 'echo "npc2 select 45" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+# os.system(command)
+# command = 'echo "npc2 moveto 62.5 51.5 741.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+# os.system(command)
+# time.sleep(1)
+# command = 'echo "npc2 select 46" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+# os.system(command)
+# command = 'echo "npc2 moveto 34.5 51.5 741.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+# os.system(command)
+# time.sleep(1)
+# command = 'echo "npc2 select 47" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+# os.system(command)
+# command = 'echo "npc2 moveto 7.5 51.5 741.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+# os.system(command)
+# time.sleep(1)
+# command = 'echo "npc2 select 48" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+# os.system(command)
+# command = 'echo "npc2 moveto 7.5 51.5 778.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+# os.system(command)
+# time.sleep(1)
+# command = 'echo "npc2 select 49" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+# os.system(command)
+# command = 'echo "npc2 moveto 34.5 51.5 778.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+# os.system(command)
+# time.sleep(1)
+# command = 'echo "npc2 select 50" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+# os.system(command)
+# command = 'echo "npc2 moveto 62.5 51.5 778.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+# os.system(command)
+# time.sleep(1)
+command = 'echo "npc2 select 51" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 1.5 50.5 755.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 52" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 25.5 43.5 760.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 53" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 1.5 50.5 764.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 54" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 44.5 43.5 759.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 59" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 71.5 51.5 769.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+command = 'echo "npc2 select 60" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+command = 'echo "npc2 moveto 71.5 51.5 750.5" | socat EXEC:"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d",pty STDIN'
+os.system(command)
+time.sleep(1)
+# ----------------------------------
