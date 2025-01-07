f40458ea551ded011eb0a51763373c62e6d36c1b
JuraRusan <rusanjura@gmail.com>
2025-01-07 20:11:53 +0200

    Больше не актуальные айди докеров

--- src/DockerUUID.js
+++
@@ -1,6 +0,0 @@
-export const dockerGMGame = '"docker attach 9697d325-5993-4715-9070-74d5cdfc0c9d"';
-export const dockerRes = '"docker attach aa146230-f115-4450-8175-e50a7c12701f"';
-export const dockerFarm = '"docker attach 013aaa06-2969-4338-9097-2caae1853916"';
-export const dockerCreative = '"docker attach 2442ba49-76b5-4943-8f5a-3a8eff02e48d"';
-export const dockerLobby = '"docker attach ad9e763d-9053-473a-8c1e-89b1cba78094"';
-export const dockerSkyBlock = '"docker attach b38d670a-bb20-443d-905b-3e52f7452f3e"';
