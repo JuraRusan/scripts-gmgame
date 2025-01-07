f6d245febcaad6cdded2751f02c631f10ea2d9e0
JuraRusan <rusanjura@gmail.com>
2025-01-07 17:03:52 +0200

    текстовый код для брони

--- src/Shops_1.21/static.js
+++ src/Shops_1.21/static.js
@@ -33,3 +33,101 @@ export const yes_sell = [
 ];
 
 export const no_sell = [{ material: "budding_amethyst", name: "Цветущий аметист", amount: 280 }];
+
+export const defaultEnchantments = {
+  mending: { lv: 1, name: "Починка" },
+  soul_speed: { lv: 3, name: "Скорость души" },
+  protection: { lv: 4, name: "Защита" },
+  projectile_protection: { lv: 4, name: "Защита от снарядов" },
+  bane_of_arthropods: { lv: 5, name: "Бич членистоногих" },
+  quick_charge: { lv: 3, name: "Быстрая перезарядка" },
+  depth_strider: { lv: 3, name: "Подводная ходьба" },
+  silk_touch: { lv: 1, name: "Шёлковое касание" },
+  unbreaking: { lv: 3, name: "Прочность" },
+  wind_burst: { lv: 3, name: "Порыв ветра" },
+  blast_protection: { lv: 4, name: "Взрывоустойчивость" },
+  channeling: { lv: 1, name: "Громовержец" },
+  fire_aspect: { lv: 2, name: "Заговор огня" },
+  flame: { lv: 1, name: "Воспламенение" },
+  punch: { lv: 2, name: "Откидывание" },
+  riptide: { lv: 3, name: "Тягун" },
+  breach: { lv: 4, name: "Пробитие" },
+  efficiency: { lv: 5, name: "Эффективность" },
+  fire_protection: { lv: 4, name: "Огнеупорность" },
+  piercing: { lv: 4, name: "Пронзающая стрела" },
+  power: { lv: 5, name: "Сила" },
+  density: { lv: 5, name: "Плотность" },
+  sweeping_edge: { lv: 3, name: "Разящий клинок" },
+  sharpness: { lv: 5, name: "Острота" },
+  loyalty: { lv: 3, name: "Верность" },
+  feather_falling: { lv: 4, name: "Невесомость" },
+  aqua_affinity: { lv: 1, name: "Подводник" },
+  luck_of_the_sea: { lv: 3, name: "Везучий рыбак" },
+  infinity: { lv: 1, name: "Бесконечность" },
+  fortune: { lv: 3, name: "Удача" },
+  impaling: { lv: 5, name: "Пронзатель" },
+  looting: { lv: 3, name: "Добыча" },
+  smite: { lv: 5, name: "Небесная кара" },
+  swift_sneak: { lv: 3, name: "Проворство" },
+  respiration: { lv: 3, name: "Подводное дыхание" },
+  multishot: { lv: 1, name: "Тройной выстрел" },
+  frost_walker: { lv: 2, name: "Ледоход" },
+  knockback: { lv: 2, name: "Отдача" },
+  lure: { lv: 3, name: "Приманка" },
+  thorns: { lv: 3, name: "Шипы" },
+  curse_of_vanishing: { lv: 1, name: "Проклятие утраты" },
+  curse_of_binding: { lv: 1, name: "Проклятие несъёмности" },
+};
+
+export const fixed_slot_minecraft = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export const armors = [
+  {
+    material: "NETHERITE_HELMET",
+    name: "Незеритовый шлем",
+    amount: 600,
+    enchantments: { aqua_affinity: 1, mending: 1, protection: 5, respiration: 4, unbreaking: 4 },
+  },
+  {
+    material: "NETHERITE_HELMET",
+    name: "Незеритовый шлем",
+    amount: 600,
+    enchantments: { aqua_affinity: 1, mending: 1, protection: 5, respiration: 4, unbreaking: 4, thorns: 3 },
+  },
+  {
+    material: "NETHERITE_HELMET",
+    name: "Незеритовый шлем",
+    amount: 600,
+    enchantments: { aqua_affinity: 1, mending: 1, projectile_protection: 5, respiration: 4, unbreaking: 4 },
+  },
+  {
+    material: "NETHERITE_HELMET",
+    name: "Незеритовый шлем",
+    amount: 600,
+    enchantments: { aqua_affinity: 1, mending: 1, projectile_protection: 5, respiration: 4, unbreaking: 4, thorns: 3 },
+  },
+  {
+    material: "NETHERITE_HELMET",
+    name: "Незеритовый шлем",
+    amount: 600,
+    enchantments: { aqua_affinity: 1, mending: 1, fire_protection: 5, respiration: 4, unbreaking: 4 },
+  },
+  {
+    material: "NETHERITE_HELMET",
+    name: "Незеритовый шлем",
+    amount: 600,
+    enchantments: { aqua_affinity: 1, mending: 1, fire_protection: 5, respiration: 4, unbreaking: 4, thorns: 3 },
+  },
+  {
+    material: "NETHERITE_HELMET",
+    name: "Незеритовый шлем",
+    amount: 600,
+    enchantments: { aqua_affinity: 1, mending: 1, blast_protection: 5, respiration: 4, unbreaking: 4 },
+  },
+  {
+    material: "NETHERITE_HELMET",
+    name: "Незеритовый шлем",
+    amount: 600,
+    enchantments: { aqua_affinity: 1, mending: 1, blast_protection: 5, respiration: 4, unbreaking: 4, thorns: 3 },
+  },
+];
